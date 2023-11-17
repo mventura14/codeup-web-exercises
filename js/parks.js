0a8a8652dced7d29c6457d49bb6fd17b6815293e
Michael Ventura <m.ventura14@yahoo.com>
2023-11-17 11:29:24 -0600

    Feat:Completed parks exercise

--- js/parks.js
+++ js/parks.js
@@ -1,6 +1,28 @@
-(() => {"use strict"
+(() => {
+    "use strict"
+    document.querySelector("#last-li-yellow").addEventListener("click", (e) => {
+        for (let ul of document.querySelectorAll("ul")) {
+            ul.children[ul.children.length - 1].style.backgroundColor = "yellow"
+        }
+    })
 
+    for (let h3 of document.querySelectorAll("h3")) {
+        h3.addEventListener("click", (e) => {
+            e.target.style.backgroundColor = "";
+            e.target
+                .parentElement
+                .nextElementSibling
+                .firstElementChild.style.fontWeight = "bold";
 
+        })
+    }
+    for (let li of document.querySelectorAll("li")){
+        li.addEventListener("click", (e)=>{
+            e.target
+                .parentElement
+                .firstElementChild.style.color="blue"
+        })
+    }
 
 
 })();
